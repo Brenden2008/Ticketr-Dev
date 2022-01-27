7088ecc558837376dd87bd3d3752912180e79376
Brenden2008 <brendenstahle@gmail.com>
2022-01-27 15:22:13 -0600

    1 file modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,104 @@
 import '../styles/var.sass';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/scss/bootstrap.scss';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Ticketr - Home</title>
+        <link
+          href="bootstrap_theme/bootstrap.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link rel="stylesheet" href="blocks.css" />
+        <link href="style.css" rel="stylesheet" />
+        <nav className="bg-dark navbar navbar-expand-lg py-lg-1 text-uppercase">
+          <div className="container">
+            {' '}
+            <a
+              className="fw-normal navbar-brand text-capitalize text-primary"
+              href="#"
+            >
+              Ticketr
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown-3"
+              aria-controls="navbarNavDropdown-3"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              {' '}
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse " id="navbarNavDropdown-3">
+              <ul className="mb-2 mb-lg-0 ms-auto navbar-nav">
+                <li className="nav-item">
+                  {' '}
+                  <a
+                    className="active nav-link px-lg-3 py-lg-4 text-capitalize"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  {' '}
+                  <a
+                    className="nav-link px-lg-3 py-lg-4 text-capitalize"
+                    href="#"
+                  >
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  {' '}
+                  <a
+                    className="nav-link px-lg-3 py-lg-4 text-capitalize"
+                    href="#"
+                  >
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item dropdown"></li>
+                <li className="nav-item">
+                  {' '}
+                  <a
+                    className="nav-link px-lg-3 py-lg-4 text-capitalize"
+                    href="#"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+              <div className="ms-lg-3">
+                <a
+                  className="btn btn-primary pe-3 ps-3 text-capitalize"
+                  href="#"
+                >
+                  Sign up
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
